a9a089131cbbfeead318d4406233db3ec34b860c
Weigang Geng <gengwg@gmail.com>
2017-06-21 11:00:40 -0700

    comment

--- hbase_readonly.sh
+++ hbase_readonly.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# to set hbase table to readonly
+# to set all hbase tables to readonly
 # echo "alter 'test',{METHOD=>'table_att', READONLY=>true}" | hbase shell
 
 for table in `python get_tables.py`; do
